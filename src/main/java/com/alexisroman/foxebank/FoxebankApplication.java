8c05cabf6699acaab9ab0cd9c95deca6a6dd3d90
Alexis Roman <romanalexisnicole06@gmail.com>
2025-10-07 22:23:14 +0800

    Initial commit

---
+++ src/main/java/com/alexisroman/foxebank/FoxebankApplication.java
@@ -0,0 +1,13 @@
+package com.alexisroman.foxebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxebankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoxebankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexisroman/foxebank/FoxebankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexisroman.foxebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoxebankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
