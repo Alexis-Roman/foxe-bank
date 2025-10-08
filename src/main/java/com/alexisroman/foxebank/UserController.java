212fb9411512b88096b50f7fa9b3b543f95d11ba
Alexis Roman <romanalexisnicole06@gmail.com>
2025-10-08 22:28:59 +0800

    add UserController with cashIn and cashOut endpoint for user transactions

---
+++ src/main/java/com/alexisroman/foxebank/UserController.java
@@ -0,0 +1,27 @@
+package com.alexisroman.foxebank;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private BankingService bankingService;
+
+    //CASH IN
+    @PostMapping("/{receiverId}/cashin")
+    public  String cashIn (@PathVariable Long receiverId, @RequestParam Long senderId, @RequestParam Double amount){
+        bankingService.cashIn(receiverId, senderId, amount);
+        return "Cash In successful! You received ₱" + amount
+                + "from user " + senderId;
+    }
+
+    //CASH OUT
+    @PostMapping("/{senderId} /cashout")
+    public  String cashOut (@PathVariable Long senderId, @RequestParam Long receiverId, @RequestParam Double amount){
+        bankingService.cashOut(senderId, receiverId, amount);
+        return "Cash Out successful! You sent ₱" + amount
+                + "to user " + senderId;
+    }
+}
