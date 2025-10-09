aff1aeae6eb1b8f3a1b8a386ee7a221133e6934f
Alexis Roman <romanalexisnicole06@gmail.com>
2025-10-09 13:33:19 +0800

    fix balance not updating respectively

--- src/main/java/com/alexisroman/foxebank/BankingService.java
+++ src/main/java/com/alexisroman/foxebank/BankingService.java
@@ -1,5 +1,6 @@
 package com.alexisroman.foxebank;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,7 @@ public class BankingService {
     @Autowired
     private TransactionRepository transactionRep;
 
+    @Transactional
     public void cashIn(Long receiverId, Long senderId, Double amount){
         User receiver = userRep.findById(receiverId).orElseThrow();
         User sender = userRep.findById(senderId).orElseThrow();
@@ -19,10 +21,14 @@ public class BankingService {
         receiver.setBalance(receiver.getBalance() + amount);
         userRep.save(receiver);
 
+        sender.setBalance(sender.getBalance() - amount);
+        userRep.save(sender);
+
         //transaction record
         transactionRep.save(new Transaction("Cash In: ", amount, sender, receiver));
     }
 
+    @Transactional
     public void cashOut(Long senderId, Long receiverId, Double amount){
         User sender = userRep.findById(senderId).orElseThrow();
         User receiver = userRep.findById(receiverId).orElseThrow();
@@ -30,6 +36,9 @@ public class BankingService {
         //user SENDS money
         sender.setBalance(sender.getBalance() - amount);
         userRep.save(sender);
+
+        receiver.setBalance(receiver.getBalance() + amount);
+        userRep.save(receiver);
 
         //transaction record
         transactionRep.save(new Transaction("Cash Out: ", amount, sender, receiver));
