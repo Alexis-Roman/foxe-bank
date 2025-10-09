e615fd7b95d874fb61dcc2666755c92255047c08
Alexis Roman <romanalexisnicole06@gmail.com>
2025-10-09 17:58:32 +0800

    add AuthService for login and signup services

--- src/main/java/com/alexisroman/foxebank/service/AuthService.java
+++ src/main/java/com/alexisroman/foxebank/service/AuthService.java
@@ -1,9 +1,59 @@
 package com.alexisroman.foxebank.service;
 
+import com.alexisroman.foxebank.dto.LoginRequest;
+import com.alexisroman.foxebank.dto.SignupRequest;
+import com.alexisroman.foxebank.entity.User;
+import com.alexisroman.foxebank.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.Optional;
 
 @Service
 public class AuthService {
+    @Autowired
+    private UserRepository userRep;
 
+    //Signing-up
+    public String signUp(SignupRequest request) {
+        if (!request.getPassword().equals(request.getConfirmPassword())){
+            return "Error: Passwords do not match";
+        }
+
+        if(Period.between(request.getBirthday(), LocalDate.now()).getYears() < 18){
+            return "You must be at least 18 y/o to create an account";
+        }
+
+        Optional<User> existing = userRep.findByEmail(request.getEmail());
+        if (existing.isPresent()){
+            return "Email already exist";
+        }
+
+        //CREATING NEW USER
+        User user   = new User(
+                request.getName(), request.getEmail(),
+                request.getBirthday(), request.getPassword(), 0.0
+        );
+
+        return "Sign In Successful!";
+    }
+
+    //Login
+    public String login(LoginRequest request){
+        Optional<User> findUser = userRep.findByEmail(request.getEmail());
+        if (findUser.isEmpty()){
+            return "User does not exist!";
+        }
+
+        User user = findUser.get();
+
+        if (!user.getPassword().equals(request.getPassword())){
+            return "Wrong Password";
+        }
+
+        return "Welcome " + user.getName();
+    }
 
 }
