2a0e148b0b10d6dd966a73ebb1e9b4f1f9753b16
Alexis Roman <romanalexisnicole06@gmail.com>
2025-10-10 11:01:09 +0800

    update frontend folder

--- src/main/java/com/alexisroman/foxebank/controller/AuthController.java
+++ src/main/java/com/alexisroman/foxebank/controller/AuthController.java
@@ -4,11 +4,9 @@ import com.alexisroman.foxebank.dto.LoginRequest;
 import com.alexisroman.foxebank.dto.SignupRequest;
 import com.alexisroman.foxebank.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173/")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
