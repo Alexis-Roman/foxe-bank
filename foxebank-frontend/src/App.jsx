8a1298445f87ba83d691788ff0cd32abb24b733e
Alexis Roman <romanalexisnicole06@gmail.com>
2025-10-10 14:56:20 +0800

    add simple ui components for login, signup, navbar, and footer

--- foxebank-frontend/src/App.jsx
+++ foxebank-frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import Login from "./pages/Login.jsx";
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import SignupPage from './components/SignupPage';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+// import './App.css';
+
+import React from "react";
+import { Box, Flex, Spacer } from "@chakra-ui/react"
+
 
 function App() {
   return (
-    <div>
-      <Login />
+    <div className='App'>
+      <LoginPage/>
+      <SignupPage/>
+      <Navbar/>
+      <Footer/>
     </div>
   );
 }
